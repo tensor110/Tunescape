4de24b858fcd36013d823b216a1387f80fc62a3b
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-12 21:31:47 +0530

    production re-commit

---
+++ Production/server/routes/playsong.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const Route = express.Router();
+let {createUser,findUser} = require("../database/controller/user/user")
+let {ADD_MUSIC_TO_MONGO,SEARCH_MUSIC_THUMBNAIL_IN_MONGO,UPDATE_PREV_MUSICS_THUMBNAIL_HASH_IN_MONGO,SEARCH_MUSIC_IN_MONGO} = require("../database/controller/music/music.js")
+
+
+Route.get('/tunescape.com/stream', (req, res) => {
+    res.render('song-fetch.ejs')
+})
+
+let cacheHashMusicLink = []
+let cacheHashMusicThumbnail = []
+
+Route.post('/getsong', async (req, res) => {
+    const GetSongName = req.body.songGETname
+    console.log(GetSongName);
+    SEARCH_MUSIC_IN_MONGO(GetSongName).then((hash)=>{
+       cacheHashMusicLink.push(hash)
+    })
+    SEARCH_MUSIC_THUMBNAIL_IN_MONGO(GetSongName).then((hash)=>{
+        cacheHashMusicThumbnail.push(hash)
+    })
+
+    res.redirect('/tunescape.com/stream/song');
+})
+
+
+Route.get('/tunescape.com/stream/song', async (req, res) => {
+    res.render("player.ejs", { hash_key: cacheHashMusicLink.pop() ,hash_img: cacheHashMusicThumbnail.pop()})
+})
+
+module.exports = Route
