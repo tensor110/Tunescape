76d48a25ea5668dbf329f487329091b82dc851a5
Saswat PB <saswat3002@gmail.com>
2023-06-29 23:27:16 +0530

    moved to production folder

---
+++ Production/client/public/scripts/player.js
@@ -0,0 +1,108 @@
+const song = document.getElementById('song');
+const progressBar = document.getElementById('seeker-bar');
+const volumeBar = document.getElementById('volume-bar');
+const songName = document.querySelector('.song-name')
+const artistName = document.querySelector('.artist-name')
+let songList= []
+let currentIndex=0;
+async function getSongList (){
+
+  let arr = await fetch('http://localhost:6969/buffer-stream-to-fetch-song');
+  console.log(arr)
+  // console.log(songList)
+  // song.src=`https://d1uzpajnrcv6ws.cloudfront.net/${songList[currentIndex].SongHash}`;
+}
+
+getSongList();
+
+
+
+
+
+
+window.addEventListener('load', function() {
+  progressBar.value = 0;
+});
+
+song.addEventListener('ended', () => {
+  playNext();
+})
+
+
+// Updates the value of the progress bar
+song.addEventListener('timeupdate', () => {
+  const currentTime = song.currentTime;
+  const duration = song.duration;
+  const progress = parseFloat(currentTime / duration) * 100;
+
+  progressBar.value = progress;
+
+});
+
+// Update music playback position when progress bar is changed
+progressBar.addEventListener('input', () => {
+  const progress = progressBar.value;
+  const duration = song.duration;
+  const currentTime = (progress / 100) * duration;
+
+  song.currentTime = currentTime;
+});
+
+// Handles play and pause
+function playPause(event) {
+
+  if (event.target.id === 'play') {
+    song.play();
+
+    document.getElementById('play').style.display = 'none';
+    document.getElementById('pause').style.display = 'inline-block';
+  }
+  else {
+    song.pause();
+    document.getElementById('play').style.display = 'inline-block';
+    document.getElementById('pause').style.display = 'none';
+  }
+
+}
+
+// Handles volume functionalities
+volumeBar.addEventListener('input', () => {
+  const volume = (volumeBar.value) / 100;
+
+  song.volume = volume;
+})
+
+function playNext() {
+  
+  if (currentIndex === (songList.length - 1)) {
+    currentIndex = 0;
+  }
+  else {
+    currentIndex++;
+  }
+  
+  song.src=`https://d1uzpajnrcv6ws.cloudfront.net/${songList[currentIndex].SongHash}`;
+  song.play();
+
+}
+
+function playPrevious() {
+  
+  if (song.currentTime < 2) {
+
+      if (currentIndex == 0) {
+          currentIndex = songList.length - 1;
+      }
+      else {
+          currentIndex--;
+      }
+      song.src = `https://d1uzpajnrcv6ws.cloudfront.net/${songList[currentIndex].SongHash}`;
+      song.play();
+  }
+  else {
+      song.currentTime = 0;
+  }
+}
+
+
+
--- Production/server/app.js
+++ Production/server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors= require('cors');
 const app = express();
+app.use(cors());
 const multer = require('multer')
 app.use(express.static(__dirname + 'Server-Frontend/public'));
 const PORT = 6969;
