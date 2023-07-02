9a0f9b843c2b85409e15476151a6d7065476a3b2
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-02 22:42:44 +0530

    host port change for deployment

--- Development/client/server.js
+++ Development/client/server.js
@@ -5,8 +5,7 @@ const ejs = require('ejs')
 app.set('view engine', 'ejs');
 app.set("views", "views");
 app.use(express.static('public'));
-
-
+const HOST = process.env.PORT || 3000;
 
 // const songList = [{
 //   title: "As it was",
@@ -77,6 +76,6 @@ app.use((req, res, next) => {
   res.status(404).render('404');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(HOST, () => {
+  console.log(`Server is running on ${HOST}`);
 });
