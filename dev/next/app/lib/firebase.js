4bffb47cbcd15c3dfc4c824cadf414c5c30bea55
Maheswar Mahapatra <maheswar2110@gmail.com>
2023-09-04 22:57:06 +0530

    dashboard completed

--- dev/next/app/lib/firebase.js
+++ dev/next/app/lib/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,6 +15,7 @@ const firebaseConfig = {
   appId:NEXT_PUBLIC_APP_ID,
   measurementId: "G-WH67XGNXS0"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
