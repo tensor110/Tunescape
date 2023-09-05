f781420635f5a47ee07832adc4539aeb3e0a973b
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-09-05 13:56:28 +0530

    fixed navbar

--- dev/next/components/Navbar.js
+++ dev/next/components/Navbar.js
@@ -36,11 +36,16 @@ const Navbar = () => {
           <i class="bx bx-x" id="close-icon"></i>
         </label>
 
-        <nav class="navbar">
-          <Link href="/stream" class="nav-item" style={{ "--i": 2 }}>Stream</Link>
-          <Link href="/dashboard" class="nav-item" style={{ "--i": 0 }}>Dashboard</Link>
-          <Link href="/about/us" class="nav-item" style={{ "--i": 1 }}>About us</Link>
-          {user?(<><Link href="/user/profile"class="nav-item " style={{ "--i": 2 }}>{user?.displayName}</Link><Link onClick={handleSignOut} href="/" class="nav-item user__text" style={{ "--i": 2 }}>Sign Out</Link></>):(<Link onClick={handleSignIn} href="/" class="nav-item" style={{ "--i": 2 }}>Login</Link>)}
+        <nav class="navbar"> <Link href="/stream" class="nav-item" 
+          style={{ "--i": 2 }}>Stream</Link> <Link 
+          href="/dashboard" class="nav-item" style={{ "--i": 0 
+          }}>Dashboard</Link> {user?(<><Link 
+          href="/user/profile"class="nav-item " style={{ "--i": 2 
+          }}>{user?.displayName}</Link><Link 
+          onClick={handleSignOut} href="/" class="nav-item 
+          user__text" style={{ "--i": 2 }}>Sign 
+          Out</Link></>):(<Link onClick={handleSignIn} href="/" 
+          class="nav-item" style={{ "--i": 2 }}>Login</Link>)}
         </nav>
       </header>
       <br />
